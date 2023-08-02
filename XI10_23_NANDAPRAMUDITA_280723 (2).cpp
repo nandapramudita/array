fb711d170e6d8f5fc1161bc9ffaeff3f3d44b6ca
nandapramudita <141210120+nandapramudita@users.noreply.github.com>
2023-08-02 13:30:50 +0700

    Add files via upload

---
+++ XI10_23_NANDAPRAMUDITA_280723 (2).cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+int main ()
+
+{
+	int i;
+	float bill [7], jumlah=0.0, rata2;
+	for (i = 0; 1 < 7; ++i);
+	{
+		cout << i+1 << ". Masukkan Angka ke-" << i+1 <<" : ";
+		cin >> bill(i);
+		while{ bil [i] > 100 || bil(i)i} <= 0}
+	   {
+			cout << "Error! Angka di antara 1 s/d 100" << endl;
+			cout << i+1 << ". Masukkan Angka ke-" << i+1 << " : ";
+			cin >> bil (i);
+     	}
+	
+	Jumlah += bil (i);			
+}
+rata2 = jumlah / 7;
+cout << "Rata2 Bilangan di atas = " << rata2 << endl;
+
+system ("PAUSE");
+return 0;
+}
